0ff3bb61cd5913e2067ebb3bfd5504c72993600f
zhangcheng <xyzc1988@163.com>
2017-11-29 16:45:52 +0800

    chapter22 验证码

---
+++ shiro-example-chapter22/src/main/java/io/github/xyzc1988/shiro/chapter22/jcaptcha/MyManageableImageCaptchaService.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2005-2012 https://github.com/zhangkaitao
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package io.github.xyzc1988.shiro.chapter22.jcaptcha;
+
+import com.octo.captcha.engine.CaptchaEngine;
+import com.octo.captcha.service.captchastore.CaptchaStore;
+import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-3-22 下午3:38
+ * <p>Version: 1.0
+ */
+public class MyManageableImageCaptchaService extends DefaultManageableImageCaptchaService {
+
+    public MyManageableImageCaptchaService(CaptchaStore captchaStore, CaptchaEngine captchaEngine, int minGuarantedStorageDelayInSeconds, int maxCaptchaStoreSize, int captchaStoreLoadBeforeGarbageCollection) {
+        super(captchaStore, captchaEngine, minGuarantedStorageDelayInSeconds, maxCaptchaStoreSize, captchaStoreLoadBeforeGarbageCollection);
+    }
+
+    public boolean hasCapcha(String id, String userCaptchaResponse) {
+        return store.getCaptcha(id).validateResponse(userCaptchaResponse);
+    }
+}
